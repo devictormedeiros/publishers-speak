300c13870405995b207bd103505a2a693a52be7b
Victor Medeiros <victor.medeiros@seox.com.br>
2023-11-03 13:58:48 -0300

    init commit

---
+++ assets/js/speakText.js
@@ -0,0 +1,55 @@
+ document.addEventListener("DOMContentLoaded", function () {
+      if (!window.speechSynthesis) {
+        console.error("Speech Synthesis não suportado neste navegador.");
+        // Desabilite o botão de alternância ou informe o usuário aqui, se necessário
+        return;
+      }
+
+      const toggleBtn = document.getElementById("toggle-speech");
+      const contentDiv = document.querySelector(".single-content");
+      let isSpeaking = false;
+      let utterance;
+
+      let availableVoices = [];
+
+      speechSynthesis.onvoiceschanged = function () {
+        availableVoices = speechSynthesis.getVoices();
+      };
+
+      function toggleSpeech() {
+        if (isSpeaking && speechSynthesis.speaking && !speechSynthesis.paused) {
+          speechSynthesis.pause();
+          toggleBtn.classList.remove("listening-content");
+        } else if (isSpeaking && speechSynthesis.paused) {
+          speechSynthesis.resume();
+          toggleBtn.classList.add("listening-content");
+        } else {
+          utterance = new SpeechSynthesisUtterance(contentDiv.innerText);
+          utterance.lang = "pt-BR";
+          utterance.volume = 1;
+          utterance.rate = 1.6;
+          utterance.pitch = 0.1;
+          utterance.voice = availableVoices[1]; // Certifique-se de que esta voz existe!
+          utterance.onstart = () => {
+            isSpeaking = true;
+            toggleBtn.classList.add("listening-content");
+          };
+
+          utterance.onend = () => {
+            isSpeaking = false;
+            toggleBtn.classList.remove("listening-content");
+          };
+
+          speechSynthesis.speak(utterance);
+        }
+      }
+
+      // Tente forçar o carregamento das vozes
+      speechSynthesis.getVoices();
+
+      toggleBtn.addEventListener("click", toggleSpeech);
+      window.addEventListener(
+        "beforeunload",
+        speechSynthesis.cancel.bind(speechSynthesis)
+      );
+    });
---
+++ includes/recordText.php
@@ -0,0 +1,23 @@
+<form action="https://staging-salvadorfm.seox.com.br/" method="GET" class="form-record-search">
+    <input type="text" name="s" class="input-search" id="input-search" aria-label="Campo de pesquisa" placeholder="Diga/digite o que você procura...">
+
+    <button type="submit" class="btn-search no-dark" title="Buscar">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--bs-white)" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+</svg>
+    </button>
+    <button id="recordText">
+        <div class="icon-record">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--bs-white)" class="bi bi-mic-fill" viewBox="0 0 16 16">
+                <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z" />
+                <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />
+            </svg>
+        </div>
+        <div class="icon-recording">
+            <span class="dot" id="dot1"></span>
+            <span class="dot" id="dot2"></span>
+            <span class="dot" id="dot3"></span>
+        </div>
+    </button>
+
+</form>
